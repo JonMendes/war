29044dd626a43651dfc491f72e32106682f4e39f
Filipe <96267462+Hazytz@users.noreply.github.com>
2024-02-01 20:50:30 -0300

    Mapa finalizado

--- Java/src/main/java/com/example/wargame/WarGame.java
+++ Java/src/main/java/com/example/wargame/WarGame.java
@@ -38,8 +38,18 @@ public class WarGame extends Application {
 
     private void initializeTerritories() {
         territories = new ArrayList<>();
-        Collections.addAll(territories, "North America", "South America", "Europe", "Africa", "Asia", "Australia");
+        territories = new ArrayList<>();
+        Collections.addAll(territories, "Alasca","Mackenzie","Vancouver","Ottawa","Labrador","Groelandia","California","Nova York","México",
+                "Venezuela","Peru","Argentina","Brasil","Islandia","Suecia","Inglaterra","Alemanha","Polonia","Moscou",
+                "Argelia","Egito","Sudao","Congo","Africa do Sul","Madagascar","Omsk","Arai","Oriente Medio","India",
+                "Vietna","China","Dudinka","Mongolia","Tachita","Siberia","Vladivostok","Japão","Sumatra","Borneu","Nova Guine","Australia");
         Collections.shuffle(territories);
+        colors = new ArrayList<>();
+        Collections.addAll(colors,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,
+                Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,
+                Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,
+                Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,
+                Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE);
     }
 
     private void initializePlayers() {
@@ -89,138 +99,179 @@ public class WarGame extends Application {
 
         //America do Norte
         //Alasca
-        gc.setFill(Color.YELLOW);
-        gc.fillRect(40, 40, 30, 30);
+        gc.setFill(colors.get(0));
+        gc.fillRect(40+50, 40, 30, 30);
+        gc.strokeRect(40+50, 40, 30, 30);
         //Mackenzie
-        gc.setFill(Color.BROWN);
-        gc.fillRect(70, 40, 30, 30);
+        gc.setFill(colors.get(1));
+        gc.fillRect(70+50, 40, 30, 30);
+        gc.strokeRect(70+50, 40, 30, 30);
         //Vancouver
-        gc.setFill(Color.LIGHTGOLDENRODYELLOW);
-        gc.fillRect(55, 70, 30, 30);
+        gc.setFill(colors.get(2));
+        gc.fillRect(55+50, 70, 30, 30);
+        gc.strokeRect(55+50, 70, 30, 30);
         //Ottawa
-        gc.setFill(Color.YELLOW);
-        gc.fillRect(85, 70, 30, 30);
+        gc.setFill(colors.get(3));
+        gc.fillRect(85+50, 70, 30, 30);
+        gc.strokeRect(85+50, 70, 30, 30);
         //Labrador
-        gc.setFill(Color.YELLOWGREEN);
-        gc.fillRect(115, 70, 30, 30);
+        gc.setFill(colors.get(4));
+        gc.fillRect(115+50, 70, 30, 30);
+        gc.strokeRect(115+50, 70, 30, 30);
         //Groelandia
-        gc.setFill(Color.YELLOW);
-        gc.fillRect(120, 20, 30, 30);
+        gc.setFill(colors.get(5));
+        gc.fillRect(120+50, 20, 30, 30);
+        gc.strokeRect(120+50, 20, 30, 30);
         //California
-        gc.setFill(Color.BROWN);
-        gc.fillRect(70, 100, 30, 30);
+        gc.setFill(colors.get(6));
+        gc.fillRect(70+50, 100, 30, 30);
+        gc.strokeRect(70+50, 100, 30, 30);
         //Nova York
-        gc.setFill(Color.GOLD);
-        gc.fillRect(100, 100, 30, 30);
+        gc.setFill(colors.get(7));
+        gc.fillRect(100+50, 100, 30, 30);
+        gc.strokeRect(100+50, 100, 30, 30);
         //México
-        gc.setFill(Color.YELLOW);
-        gc.fillRect(90, 130, 30, 30);
+        gc.setFill(colors.get(8));
+        gc.fillRect(90+50, 130, 30, 30);
+        gc.strokeRect(90+50, 130, 30, 30);
 
         //America do Sul
         //Venezuela
-        gc.setFill(Color.RED);
-        gc.fillRect(120, 160, 30, 30);
+        gc.setFill(colors.get(9));
+        gc.fillRect(120+50, 160, 30, 30);
+        gc.strokeRect(120+50, 160, 30, 30);
         //Peru
-        gc.setFill(Color.DARKRED);
-        gc.fillRect(105, 190, 30, 30);
+        gc.setFill(colors.get(10));
+        gc.fillRect(105+50, 190, 30, 30);
+        gc.strokeRect(105+50, 190, 30, 30);
         //Argentina
-        gc.setFill(Color.MEDIUMVIOLETRED);
-        gc.fillRect(105, 220, 30, 30);
+        gc.setFill(colors.get(11));
+        gc.fillRect(105+50, 220, 30, 30);
+        gc.strokeRect(105+50, 220, 30, 30);
         //Brasil
-        gc.setFill(Color.ORANGERED);
-        gc.fillRect(135, 190, 30, 50);
+        gc.setFill(colors.get(12));
+        gc.fillRect(135+50, 190, 30, 50);
+        gc.strokeRect(135+50, 190, 30, 50);
 
         //Europa
         //Islandia
-        gc.setFill(Color.BLUE);
-        gc.fillRect(170, 50, 10, 10);
+        gc.setFill(colors.get(13));
+        gc.fillRect(170+50, 50, 10, 10);
+        gc.strokeRect(170+50, 50, 10, 10);
         //Suecia
-        gc.setFill(Color.DARKBLUE);
-        gc.fillRect(210, 30, 30, 30);
+        gc.setFill(colors.get(14));
+        gc.fillRect(210+50, 30, 30, 30);
+        gc.strokeRect(210+50, 30, 30, 30);
         //Inglaterra
-        gc.setFill(Color.CADETBLUE);
-        gc.fillRect(190, 60, 20, 20);
+        gc.setFill(colors.get(15));
+        gc.fillRect(190+50, 60, 20, 20);
+        gc.strokeRect(190+50, 60, 20, 20);
         //Alemanha
-        gc.setFill(Color.BLUE);
-        gc.fillRect(210, 60, 10, 20);
+        gc.setFill(colors.get(16));
+        gc.fillRect(210+50, 60, 10, 20);
+        gc.strokeRect(210+50, 60, 10, 20);
         //Polonia
-        gc.setFill(Color.DODGERBLUE);
-        gc.fillRect(220, 60, 20, 20);
+        gc.setFill(colors.get(17));
+        gc.fillRect(220+50, 60, 20, 20);
+        gc.strokeRect(220+50, 60, 20, 20);
         //Moscou
-        gc.setFill(Color.BLUEVIOLET);
-        gc.fillRect(240, 30, 30, 40);
+        gc.setFill(colors.get(18));
+        gc.fillRect(240+50, 30, 30, 40);
+        gc.strokeRect(240+50, 30, 30, 40);
 
         //Africa
         //Argelia
-        gc.setFill(Color.PINK);
-        gc.fillRect(210, 130, 30, 30);
+        gc.setFill(colors.get(19));
+        gc.fillRect(210+50, 130, 30, 30);
+        gc.strokeRect(210+50, 130, 30, 30);
         //Egito
-        gc.setFill(Color.DEEPPINK);
-        gc.fillRect(240, 120, 30, 20);
+        gc.setFill(colors.get(20));
+        gc.fillRect(240+50, 120, 30, 20);
+        gc.strokeRect(240+50, 120, 30, 20);
         //Sudao
-        gc.setFill(Color.HOTPINK);
-        gc.fillRect(240, 140, 30, 20);
+        gc.setFill(colors.get(21));
+        gc.fillRect(240+50, 140, 30, 20);
+        gc.strokeRect(240+50, 140, 30, 20);
         //Congo
-        gc.setFill(Color.DEEPPINK);
-        gc.fillRect(220, 160, 30, 30);
+        gc.setFill(colors.get(22));
+        gc.fillRect(220+50, 160, 30, 30);
+        gc.strokeRect(220+50, 160, 30, 30);
         //Africa do Sul
-        gc.setFill(Color.PINK);
-        gc.fillRect(230, 190, 30, 30);
+        gc.setFill(colors.get(23));
+        gc.fillRect(230+50, 190, 30, 30);
+        gc.strokeRect(230+50, 190, 30, 30);
         //Madagascar
-        gc.setFill(Color.HOTPINK);
-        gc.fillRect(260, 170, 10, 10);
+        gc.setFill(colors.get(24));
+        gc.fillRect(260+50, 170, 10, 10);
+        gc.strokeRect(260+50, 170, 10, 10);
 
         //Asia
         //Omsk
-        gc.setFill(Color.GREEN);
-        gc.fillRect(270, 30, 30, 30);
+        gc.setFill(colors.get(25));
+        gc.fillRect(270+50, 30, 30, 30);
+        gc.strokeRect(270+50, 30, 30, 30);
         //Arai
-        gc.setFill(Color.DARKGREEN);
-        gc.fillRect(270, 60, 30, 30);
+        gc.setFill(colors.get(26));
+        gc.fillRect(270+50, 60, 30, 30);
+        gc.strokeRect(270+50, 60, 30, 30);
         //Oriente Medio
-        gc.setFill(Color.LIGHTGREEN);
-        gc.fillRect(270, 90, 40, 30);
+        gc.setFill(colors.get(27));
+        gc.fillRect(270+50, 90, 40, 30);
+        gc.strokeRect(270+50, 90, 40, 30);
         //India
-        gc.setFill(Color.GREEN);
-        gc.fillRect(300, 90, 30, 30);
+        gc.setFill(colors.get(28));
+        gc.fillRect(300+50, 90, 30, 30);
+        gc.strokeRect(300+50, 90, 30, 30);
         //Vietna
-        gc.setFill(Color.DARKGREEN);
-        gc.fillRect(330, 90, 15, 35);
+        gc.setFill(colors.get(29));
+        gc.fillRect(330+50, 90, 15, 35);
+        gc.strokeRect(330+50, 90, 15, 35);
         //China
-        gc.setFill(Color.LIGHTGREEN);
-        gc.fillRect(300, 60, 60, 30);
+        gc.setFill(colors.get(30));
+        gc.fillRect(300+50, 60, 60, 30);
+        gc.strokeRect(300+50, 60, 60, 30);
         //Dudinka
-        gc.setFill(Color.DARKGREEN);
-        gc.fillRect(300, 20, 15, 40);
+        gc.setFill(colors.get(31));
+        gc.fillRect(300+50, 20, 15, 40);
+        gc.strokeRect(300+50, 20, 15, 40);
         //Mongolia
-        gc.setFill(Color.GREEN);
-        gc.fillRect(315, 45, 45, 15);
+        gc.setFill(colors.get(32));
+        gc.fillRect(315+50, 45, 45, 15);
+        gc.strokeRect(315+50, 45, 45, 15);
         //Tachita
-        gc.setFill(Color.LIGHTGREEN);
-        gc.fillRect(315, 30, 45, 15);
+        gc.setFill(colors.get(33));
+        gc.fillRect(315+50, 30, 45, 15);
+        gc.strokeRect(315+50, 30, 45, 15);
         //Siberia
-        gc.setFill(Color.DARKGREEN);
-        gc.fillRect(315, 0, 45, 30);
+        gc.setFill(colors.get(34));
+        gc.fillRect(315+50, 0, 45, 30);
+        gc.strokeRect(315+50, 0, 45, 30);
         //Vladivostok
-        gc.setFill(Color.GREENYELLOW);
-        gc.fillRect(360, 30, 50, 50);
+        gc.setFill(colors.get(35));
+        gc.fillRect(360+50, 30, 50, 50);
+        gc.strokeRect(360+50, 30, 50, 50);
         //Japão
-        gc.setFill(Color.GREEN);
-        gc.fillRect(370, 90, 10, 30);
+        gc.setFill(colors.get(36));
+        gc.fillRect(370+50, 90, 10, 30);
+        gc.strokeRect(370+50, 90, 10, 30);
 
         //Oceania
         //Sumatra
-        gc.setFill(Color.ORANGE);
-        gc.fillRect(320, 160, 10, 30);
+        gc.setFill(colors.get(37));
+        gc.fillRect(320+50, 160, 10, 30);
+        gc.strokeRect(320+50, 160, 10, 30);
         //Borneu
-        gc.setFill(Color.ORANGERED);
-        gc.fillRect(340, 160, 20, 20);
+        gc.setFill(colors.get(38));
+        gc.fillRect(340+50, 160, 20, 20);
+        gc.strokeRect(340+50, 160, 20, 20);
         //Nova Guine
-        gc.setFill(Color.DARKORANGE);
-        gc.fillRect(370, 170, 30, 10);
+        gc.setFill(colors.get(39));
+        gc.fillRect(370+50, 170, 30, 10);
+        gc.strokeRect(370+50, 170, 30, 10);
         //Australia
-        gc.setFill(Color.ORANGE);
-        gc.fillRect(340, 200, 40, 40);
+        gc.setFill(colors.get(40));
+        gc.fillRect(340+50, 200, 40, 40);
+        gc.strokeRect(340+50, 200, 40, 40);
 
         // Adicione mais desenhos conforme necessário
     }
